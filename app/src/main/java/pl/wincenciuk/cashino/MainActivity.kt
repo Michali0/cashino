9ab71e4dad5f28e8912979308b5242f2acab005d
MichaliLaptop <plmichali9@gmail.com>
2023-06-09 22:15:08 +0200

    Dodane podstawowe logowanie

--- app/src/main/java/pl/wincenciuk/cashino/MainActivity.kt
+++ app/src/main/java/pl/wincenciuk/cashino/MainActivity.kt
@@ -1,9 +1,18 @@
 package pl.wincenciuk.cashino
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import pl.wincenciuk.cashino.databinding.ActivityMainBinding
 
 private lateinit var btnLogin: Button
@@ -11,15 +20,38 @@ private lateinit var btnLogin: Button
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
         binding.btnLogin.setOnClickListener {
-            val intent = Intent (this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
+            val email = findViewById<EditText>(R.id.loginEt).text.toString()
+            val pass = findViewById<EditText>(R.id.textPasswordEt).text.toString()
+            auth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        findViewById<EditText>(R.id.textPasswordEt).text.clear()
+                        val intent = Intent (this@MainActivity, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Nieudane logowanie",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        findViewById<EditText>(R.id.textPasswordEt).text.clear()
+                    }
+                }
+
         }
     }
 }
